d2eded285c6d018805114fdaa86f84f34bc9e4b1
abdullahishtiaq999 <abdullahmirzaishtiaq69@gmail.com>
2026-07-19 16:19:11 +0500

    linear search algorithme done

---
+++ js/algorithms/app.js
@@ -0,0 +1,25 @@
+// Linear Search Algorithm ( with sorted and unsorted data )
+
+const array = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+const target = 70;
+
+// function findNum(arr, target) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === target) {
+//             return i;
+//         };
+//     };
+
+//     return -1;
+// };
+
+// const result = findNum(array, target);
+
+// if (result !== -1) {
+//     console.log(`Target Number Found at ${result}`);
+// } else {
+//     console.log("Target Number is Not Found");
+// };
+
+// Linear Search Algorithm with Unsorted Data
