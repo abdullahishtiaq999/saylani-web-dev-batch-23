1a8d4b34ea14efd88cf4cc2edd263b5469743c34
abdullahishtiaq999 <abdullahmirzaishtiaq69@gmail.com>
2026-07-22 21:18:39 +0500

    js assignment 01 done

---
+++ js/SMIT-assignments/class-assignments/assignment-01/app.js
@@ -0,0 +1,96 @@
+// task 1
+
+// var userName = prompt("Please Enter Your Name");
+// var userAge = +prompt("Please Enter Your Age");
+// var userCity = prompt("Please Enter Your City");
+
+// alert("Hello" + " " + userName);
+// alert("You are" + " " + userAge + " " + "years old.");
+// alert("You Live in" + " " + userCity);
+
+// task 2
+
+// var num1 = +prompt("Enter a Number");
+// var num2 = +prompt("Enter a second Number");
+// var sum = num1 + num2;
+
+// alert("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sum);
+
+// task 3
+
+// var num1 = +prompt("Please Enter a Number");
+// var num2 = +prompt("Please Enter a second Number");
+// var add = num1 + num2;
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+
+// alert("Addition of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + add);
+// alert("Subtraction of" + " " + num1 + " " + "with" + " " + num2 + " " + "is" + " " + sub);
+// alert("Multiplication of" + " " + num1 + " " + "with" + " " + num2 + " " + "is" + " " + mul);
+// alert("Division of" + " " + num1 + " " + "with" + " " + num2 + " " + "is" + " " + div);
+
+// task 4
+
+// var length = +prompt("Please Enter a Length of a rectangle");
+// var width = +prompt("Please Enter a Width of a rectangle");
+// var area = length * width;
+
+// alert("Area of a rectangle is" + " " + area);
+
+// task 5
+
+// pie = 3.142
+// circumference = 2(pie)r
+
+// var radius = +prompt("Please Enter a radius of a circle");
+// var pie = 3.142;
+// var circumference = 2 * pie * radius;
+
+// alert("Circumference of a Circle is" + " " + circumference);
+
+// task 6
+
+// var userName = prompt("Please Enter Your Name");
+// var engMarks = +prompt("Please Enter Your English Marks");
+// var mathMarks = +prompt("Please Enter Your Mathematics Marks");
+// var sciMarks = +prompt("Please Enter Your Science Marks");
+
+// var totalMarks = 300;
+// var obtainedMarks = engMarks + mathMarks + sciMarks;
+
+// alert("Name:" + " " + userName + " " + "\nTotal Marks:" + " " + totalMarks + "\n" + "Obtained Marks:" + " " + obtainedMarks);
+
+// task 7
+
+// var userAge = +prompt("Please Enter Your Current Age");
+// var afterAge = userAge + 10;
+
+// alert("You will be" + " " + afterAge + " " + "after 10 years");
+
+// task 8
+
+// var USD = +prompt("Enter an amount in USD");
+// var USDtoPKR = USD * 285;
+
+// alert(USD + " " + "Dollars in pkr is" + " " + USDtoPKR + "rs.");
+
+// task 9
+
+// var userName = prompt("Please Enter Your Name");
+// var favProgLang = prompt("Please Enter Your Programming Language");
+
+// alert("Hello" + " " + userName);
+// alert("Your Favourite Programming Language is" + " " + favProgLang);
+
+// task 10
+
+var prodName = prompt("Please Enter Your Product Name");
+var prodPrice = +prompt("Please enter an amount of a product");
+var prodQuantity = +prompt("Please Enter a quantity of a product");
+var total = prodPrice * prodQuantity;
+
+alert("Product:" + " " + prodName);
+alert("Price:" + " " + prodPrice);
+alert("Quantity:" + " " +prodQuantity);
+alert("Total:" + " " + total);
