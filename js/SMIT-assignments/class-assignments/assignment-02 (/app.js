6a17bec645dbbe77ac243ad4c87e7f2276667aec
abdullahishtiaq999 <abdullahmirzaishtiaq69@gmail.com>
2026-07-23 01:39:45 -0700

    js assignment 1 and 2 done

---
+++ js/SMIT-assignments/class-assignments/assignment-02 (/app.js
@@ -0,0 +1,158 @@
+// task 1
+
+// var num = +prompt("Please Enter a Number");
+
+// if (num > 0) {
+//     alert(num + " " + "is Positive");
+// } else if (num < 0) {
+//     alert(num + " " + "is Negative");
+// } else if (num === 0) {
+//     alert(num + " " + "is Zero");
+// } else {
+//     alert("Please Enter a Valid Number");
+// }
+
+// task 2
+
+// var num = +prompt("Please Enter a Number");
+
+// if (num % 2 === 0) {
+//     alert(num + " " + "is even");
+// } else {
+//     alert(num + " " + "is odd");
+// }
+
+// task 3
+
+// var userAge = +prompt("Please Enter Your Age");
+
+// if (userAge >= 18) {
+//     alert("You are eligible for vote")
+// } else {
+//     alert("You are not eligible for vote")
+// };
+
+// task 4
+
+// var marks = +prompt("Please Enter Your Marks");
+
+// if (marks >= 50) {
+//     alert("You are passed")
+// } else {
+//     alert("Sorry You are Fail")
+// };
+
+// task 5
+
+// var num = +prompt("Please Enter a Number");
+
+// if (num > 100) {
+//     alert("Large Number")
+// } else {
+//     alert("Small Number")
+// };
+
+// task 6
+
+// var pass = prompt("Please Enter a Password");
+
+// if (pass === "admin123") {
+//     alert("Login Successfull")
+// } else {
+//     alert("Incorrect Password")
+// };
+
+// task 7
+
+// var productPrice = 1000;
+// var userMoney = +prompt("Please Enter Your Amount of Money");
+
+// if (userMoney >= productPrice) {
+//     alert("Purchase Successful")
+// } else {
+//     alert("Insufficient Balance")
+// };
+
+// task 8
+
+// var temp = +prompt("Please Enter a Temprature centigrade");
+
+// if (temp > 30) {
+//     alert("It's Hot Today")
+// } else {
+//     alert("Weather is Pleasent")
+// };
+
+// task 9
+
+// var num = +prompt("Please Enter a Number");
+
+// if (num % 5 === 0) {
+//     alert(num + " " + "is divisible by 5")
+// } else {
+//     alert(num + " " + "is not divisible by 5")
+// }
+
+// task 10
+
+// var traffLight = prompt("Enter a Traffic Light color");
+
+// if (traffLight.toLocaleLowerCase() === "red") {
+//     alert("Stop")
+// } else if (traffLight.toLocaleLowerCase() === "yellow") {
+//     alert("Get Ready")
+// } else if (traffLight.toLocaleLowerCase() === "green") {
+//     alert("Go Now")
+// } else {
+//     alert("Please Enter only red, green and blue color")
+// };
+
+// task 11
+
+// var userName = prompt("Please Enter Your User Name");
+
+// if (userName.toLocaleLowerCase() === "admin") {
+//     alert("Welcome Admin")
+// } else {
+//     alert("Access Denied")
+// };
+
+// task 12
+
+// var userHeight = +prompt("Please Enter Your Height in centimetres");
+
+// if (userHeight >= 170) {
+//     alert("Tall")
+// } else {
+//     alert("Average Height")
+// };
+
+// task 13
+
+// var isLoggedIn = true;
+
+// if (isLoggedIn) {
+//     alert("Welcome Back")
+// } else {
+//     alert("Please Log In")
+// };
+
+// task 14
+
+// var userAge = +prompt("Please Enter Your Age");
+
+// if (userAge >= 60) {
+//     alert("Senior Citizen")
+// } else {
+//     alert("Junior Citizen")
+// };
+
+// task 15
+
+// var isShopOpen = true;
+
+// if (isShopOpen) {
+//     alert("Shop is Open")
+// } else {
+//     alert("Shop is Closed")
+// };
