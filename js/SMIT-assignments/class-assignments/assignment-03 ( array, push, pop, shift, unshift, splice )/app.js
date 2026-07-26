0f16715adbd475c9c8a0c5c0a9cfaf73338fcfca
abdullahishtiaq999 <abdullahmirzaishtiaq69@gmail.com>
2026-07-26 01:14:04 -0700

    13 tasks of array done

---
+++ js/SMIT-assignments/class-assignments/assignment-03 ( array, push, pop, shift, unshift, splice )/app.js
@@ -0,0 +1,82 @@
+// task 1
+
+// var arr = [];
+
+// arr.push("Apple" , "Mango" , "Banana" , "Orange" , "Grapes");
+// console.log(arr);
+
+// task 2
+
+// var fruits = ["Apple" , "Mango" , "Banana" , "Orange" , "Grapes"];
+
+// fruits.pop();
+// console.log(fruits);
+
+// task 3
+
+// var cities = ["karachi", "lahore" , "islamabad" , "faislabad" , "haidrabad"];
+
+// cities.shift();
+// console.log(cities);
+
+// task 4
+
+// var subjects = ["English", "Mathematics" , "Biology"];
+
+// subjects.unshift("Physics");
+// console.log(subjects);
+
+// task 5
+
+// var fruits = ["Apple" , "Mango" , "Banana" , "Orange" , "Grapes"];
+// console.log(fruits.length);
+
+// task 6
+
+// var names = ["Abdullah" , "Ali" , "Ahmed" , "Bilal" , "Danial"];
+// names.push("Mujtaba" , "Muhammad");
+// console.log(names);
+
+// task 7
+
+// var numbers = [1,2,3,4,5];
+// numbers.pop();
+// numbers.pop();
+// console.log(numbers);
+
+// task 8
+
+// var colors = ["red" , "green" , "blue" , "purple" , "orange"];
+// colors.shift();
+// colors.unshift("black");
+// console.log(colors);
+
+// task 9
+
+// var tech = ["HTML" , "CSS" , "JavaScript"];
+// tech.push("React");
+// console.log(tech);
+
+// task 10
+
+// var names = ["Ali" , "Ahmed" , "Usman", "Hamza"];
+// names.splice(1,1);
+// console.log(names);
+
+// task 11
+
+// var fruits = ["Apple" , "Banana" , "Mango"];
+// fruits.splice(2,0,"Orange");
+// console.log(fruits);
+
+// task 12
+
+// var nums = [10,20,30,40,50];
+// nums.splice(2,2);
+// console.log(nums);
+
+// task 13
+
+// var alph = ["a","b","c","d"];
+// alph.splice(2,0,"x");
+// console.log(alph);
