3e6a7a572b7c8306c039b2256c259da7d655f490
abdullahishtiaq999 <abdullahmirzaishtiaq69@gmail.com>
2026-07-27 11:17:53 -0700

    10 tasks of loops done

---
+++ js/SMIT-assignments/class-assignments/assignment-04 ( loops )/app.js
@@ -0,0 +1,75 @@
+// task 1
+
+// for(var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// task 2
+
+// for(var i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+// task 3
+
+// for (var i = 1; i <= 5; i++) {
+//     console.log(i, "Hello JavaScript");
+// }
+
+// task 4
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// };
+
+// task 5
+
+// var marks = [50, 60, 70, 80];
+// var totalMarks = 0;
+// for (var i = 0; i < marks.length; i++) {
+//     totalMarks += marks[i]
+// };
+
+// console.log(totalMarks);
+
+// task 6
+
+// var marks = [50, 60, 70, 80];
+// var totalMarks = 0;
+// var percentage = null;
+// for (var i = 0; i < marks.length; i++) {
+//     totalMarks += marks[i];
+// };
+
+// percentage = totalMarks / marks.length;
+// console.log(percentage);
+
+// task 7
+
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+// for (var i = colors.length - 1; i >= 0; i--) {
+//     console.log(colors[i]);
+// };
+
+// task 8
+
+// var nums = [5, 10, 15, 20];
+// for (var i = 0; i < nums.length; i++) {
+//     console.log(nums[i] + " at " + i + " index.");
+// }
+
+// task 9
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// console.log(cities.length);
+
+// task 10
+
+// var nums = [2, 5, 8, 11, 14, 17, 20];
+// for (var i = 0; i < nums.length; i++) {
+//     if (nums[i] % 2 === 0) {
+//         console.log("even number is", nums[i]);
+//     };
+// };
